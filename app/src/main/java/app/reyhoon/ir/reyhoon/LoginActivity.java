e56b73bb1f755816c3a64ce32206bb97c4b1e195
behdad222 <behdad.222@gmail.com>
2015-05-05 00:31:49 +0430

    Click listner

--- app/src/main/java/app/reyhoon/ir/reyhoon/LoginActivity.java
+++ app/src/main/java/app/reyhoon/ir/reyhoon/LoginActivity.java
@@ -4,13 +4,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText userName;
+    private EditText password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userName = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(this);
+
     }
 
     @Override
@@ -33,5 +47,10 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
