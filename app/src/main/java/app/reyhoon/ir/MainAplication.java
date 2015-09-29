8185340a002d1b1eb1f18c4776a647f0327e8c3e
behdad222 <behdad.222@gmail.com>
2015-09-29 16:57:31 +0330

    add rtl suport

--- app/src/main/java/app/reyhoon/ir/MainAplication.java
+++ app/src/main/java/app/reyhoon/ir/MainAplication.java
@@ -3,14 +3,24 @@ package app.reyhoon.ir;
 
 import android.app.Application;
 import android.content.ContextWrapper;
+import android.content.res.Configuration;
 
 import com.pixplicity.easyprefs.library.Prefs;
+
+import java.util.Locale;
 
 public class MainAplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Locale locale = new Locale("fa");
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
 
         // Initialize the Prefs class
         new Prefs.Builder()
